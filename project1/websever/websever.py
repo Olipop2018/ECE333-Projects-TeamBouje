83116113ed8b9e4530db3956c9f1f5e7316ad114
Olivier Innocent <olivier.x2011@hotmail.com>
2019-09-22 17:10:10 -0500

    File addes and update
    added 2 html files for future testing. updated  the current python file with comments explaining briefly what each line i added do

--- project1/websever/websever.py
+++ project1/websever/websever.py
@@ -4,31 +4,50 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 #Prepare a sever socket
 #Fill in start
+# this will be local host IP: 127.0.0.1
+HOST, PORT = '', 8888
+
+# prevents waiting for accepting multiple requests
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+# bind the sockect and port number
+serverSocket.bind((HOST, PORT))
+#server starts listening for request, 
+#in this case will listen to 5 request before it it stops listening 
+serverSocket.listen(5)
+
+
 #Fill in end
 while True:
- #Establish the connection
- print('Ready to serve...')
- connectionSocket, addr = #Fill in start #Fill in end
- try:
- message = #Fill in start #Fill in end
- filename = message.split()[1]
- f = open(filename[1:])
- outputdata = #Fill in start #Fill in end
- #Send one HTTP header line into socket
- #Fill in start
- #Fill in end
- #Send the content of the requested file to the client
- for i in range(0, len(outputdata)):
- connectionSocket.send(outputdata[i].encode())
- connectionSocket.send("\r\n".encode())
-
- connectionSocket.close()
- except IOError:
- #Send response message for file not found
- #Fill in start 
- #Fill in end
- #Close client socket
- #Fill in start
- #Fill in end
+    print('Ready to serve')
+    connectionSocket, addr = serverSocket.accept() #Fill in start #Fill in end
+    #Establish the connection
+    try:
+        # writes the clients request in to message (the file it wants to access)
+        message = connectionSocket.recv(1024)
+  
+        filename = message.split()[1]
+        f = open(filename[1:])
+        # reads the file
+        outputdata = f.read() #Fill in start #Fill in end
+        f.close() #close file to free resources
+        #Send one HTTP header line into socket
+        #Fill in start
+        connectionSocket.send('HTTP/1.1 200 OK\r\n') # hhtp ok messae
+        connectionSocket.send("Content-Type: text/html\r\n\r\n")# content type
+        #Fill in end
+        #Send the content of the requested file to the client
+        for i in range(0, len(outputdata)):
+              connectionSocket.send(outputdata[i].encode())
+        connectionSocket.send("\r\n".encode())
+        connectionSocket.close()
+    except IOError:
+        #Send response message for file not found
+        #Fill in start
+        
+        #Fill in end
+        #Close client socket
+        #Fill in start
+        connectionSocket.close()
+        #Fill in end
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data 
