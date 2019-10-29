1476264297cef841b8747ff4479a8525559b87c2
Rachel Estilo <rachelestilo@me.com>
2019-10-29 10:17:35 -0500

    Proj2 - Added calculations for min, max, and avg RTTs

--- project2/Client/Client.py
+++ project2/Client/Client.py
@@ -11,21 +11,41 @@ bufferSize = 1024
 
 ClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 ClientSocket.settimeout(1)
+
+# inits for min, max, and avg RTTs
+maxRTT = 0
+minRTT = 1
+compareRTT = 0
+totalRTT = 0
+avgRTT = 0
+count = 0
+
 # Send to server using created UDP socket
 for i in range(1,11):
-    beginTime= time.time()
-    seq= i
+    beginTime = time.time()
+    seq = i
     print("PING {} {}".format(i, beginTime))
     ClientSocket.sendto(WhatToSend, serverAddressPort)
     try:
         ServerMsg = ClientSocket.recvfrom(bufferSize)
     except socket.timeout:
-        print("OOOOOOOOOOOOOOOOOHHHHHHHHHH NOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!! Request Timed out, Try again...")
+        print("   > OOOOOOOOOOOOOOOOOHHHHHHHHHH NOOOOOOOOOOOOOOOOOOOOOOOOOO!!!!!!!!!!!!! Request Timed out, Try again...")
         continue
     endTime= time.time()
     if(ServerMsg[0] !=''):
         rcvmsg= ServerMsg[0].decode("utf-8")
         RTT= endTime- beginTime
-        msg = "Message from Server {} and RTT = {} seconds".format(rcvmsg, RTT)
+        compareRTT = RTT
+        msg = "   > Message from Server {} and RTT = {} seconds".format(rcvmsg, RTT)
         print(msg)
+        # Getting min/max RTTs + total RTTs for avg
+        if (compareRTT > maxRTT):
+            maxRTT = compareRTT
+        elif (compareRTT < minRTT):
+            minRTT = compareRTT
+        totalRTT += RTT
+        count += 1
+avgRTT = totalRTT / count   # get avg RTT
+msg2 = "\n >>> REPORT: Min RTT = {} seconds, Max RTT = {} seconds, Avg RTT = {} seconds".format(minRTT, maxRTT, avgRTT)
+print(msg2)
 
